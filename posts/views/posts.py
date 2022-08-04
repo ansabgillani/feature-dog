f136ffd07c67f9ba9e5ee9d8c9c159f0fb56b637
ansabgillani <syedansab500@gmail.com>
2022-08-04 14:17:29 +0500

    refactor code

--- posts/views/posts.py
+++ posts/views/posts.py
@@ -1,15 +1,11 @@
 from users.models import CustomerProfile, Organization, User
 from posts.serializers import (
-    CommentSerializer,
     PostSerializer,
     Post,
     UpvoteSerializer,
-    CommentUpvoteSerializer,
 )
 from posts.models import (
     Post,
-    CommentUpvote,
-    Comment,
     Upvote,
     PostTag,
     Tag,
@@ -22,63 +18,9 @@ from rest_framework import (
 )
 
 
-class CommentRetriveView(generics.RetrieveAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-
-    def get(self, request, comment_pk, post_pk, organization_slug):
-        try:
-            organization = Organization.objects.get(slug=organization_slug)
-            post = Post.objects.get(receiver=organization, pk=post_pk)
-            comment = Comment.objects.get(post=post, pk=comment_pk)
-            serializer = CommentSerializer(comment)
-            return response.Response(data=serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return response.Response(
-                data={
-                    'error': str(e)
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-
-class CommentListCreateView(generics.ListCreateAPIView):
-    queryset = Comment.objects.all()
-    serializer_class = CommentSerializer
-    lookup_field = 'comment_key'
-
-    def post(self, request, organization_slug, post_pk):
-        try:
-            data = request.data
-            organization = Organization.objects.get(slug=organization_slug)
-            post = Post.objects.get(receiver=organization.pk, pk=post_pk)
-            sender = User.objects.get(username=data.get('sender'))
-            comment = Comment.objects.create(
-                body=data.get('body'),
-                sender=sender,
-                post=post,
-            )
-            serializer = CommentSerializer(comment)
-            return response.Response(
-                data=serializer.data,
-                status=status.HTTP_201_CREATED,
-            )
-        except Exception as e:
-            return response.Response(
-                data={
-                    'error': str(e)
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-
 class PostListCreateView(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     queryset = Post.objects.all()
-
-    # def get_queryset(self):
-    #     self.kwargs.get
-    #     return super().get_queryset()
 
     def get(self, request, organization_slug):
         try:
@@ -125,12 +67,6 @@ class PostListCreateView(generics.ListCreateAPIView):
                     p_tag.save()
             serializer = PostSerializer(post)
             return response.Response(data=serializer.data, status=status.HTTP_201_CREATED)
-            # data["receiver_slug"] = organization_slug
-            # serializer = PostSerializer(data=data)
-            # if serializer.validate():
-            #     serializer.save()
-
-            #     return response.Response(data=serializer.data, status=status.HTTP_201_CREATED)
         except Exception as e:
             return response.Response(
                 data={
@@ -168,7 +104,7 @@ class UpvoteListCreateView(generics.ListCreateAPIView):
             organization = Organization.objects.get(slug=organization_slug)
             post = Post.objects.get(receiver=organization, id=post_pk)
             upvote = Upvote.objects.get(post=post)
-            serializer = UpvoteSerializerß(upvote)
+            serializer = UpvoteSerializer(upvote)
             return response.Response(data=serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
             return response.Response(
@@ -190,52 +126,6 @@ class UpvoteListCreateView(generics.ListCreateAPIView):
                 post=post
             )
             serializer = UpvoteSerializer(upvote)
-            return response.Response(
-                data=serializer.data,
-                status=status.HTTP_201_CREATED,
-            )
-        except Exception as e:
-            return response.Response(
-                data={
-                    'error': str(e)
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-
-class CommentUpvoteListCreateView(generics.ListCreateAPIView):
-    queryset = CommentUpvote.objects.all()
-    serializer_class = CommentUpvoteSerializer
-
-    def get(self, request, comment_pk, post_pk, organization_slug):
-        try:
-            organization = Organization.objects.get(slug=organization_slug)
-            post = Post.objects.get(receiver=organization, pk=post_pk)
-            comment = Comment.objects.get(post=post, pk=comment_pk)
-            comment_upvote = CommentUpvote.objects.filter(comment=comment)
-            serializer = CommentUpvoteSerializer(comment_upvote, many=True)
-            return response.Response(data=serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            return response.Response(
-                data={
-                    'error': str(e)
-                },
-                status=status.HTTP_404_NOT_FOUND
-            )
-
-    def post(self, request, comment_pk, organization_slug, post_pk):
-        try:
-            data = request.data
-            organization = Organization.objects.get(slug=organization_slug)
-            post = Post.objects.get(receiver=organization, pk=post_pk)
-            comment = Comment.objects.get(post=post, pk=comment_pk)
-            upvote_by = User.objects.get(username=data['upvote_by'])
-
-            comment_upvote = CommentUpvote.objects.create(
-                upvote_by=upvote_by,
-                comment=comment
-            )
-            serializer = CommentUpvoteSerializer(comment_upvote)
             return response.Response(
                 data=serializer.data,
                 status=status.HTTP_201_CREATED,
